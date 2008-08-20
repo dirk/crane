f0fddf303cd333217786298c0ce3a024970f8223
Dirk Gadsden <dirk@esherido.com>
2008-08-20 19:04:37 -0400

    Fields concept

--- app/models/user.php
+++ app/models/user.php
@@ -1,15 +1,4 @@
 <?php
-class Fields {
-	var $string = 'a string';
-	var $password = 'a password';
-	var $url = 'a URL';
-}
-class Model {
-	var $fields, $structure;
-	function Model(){
-		$this->fields = new Fields();
-	}
-}
 class User extends Model {
 	function User(){
 		// Load default model functions and accesories from the parent.
---
+++ system/models.php
@@ -0,0 +1,32 @@
+<?php
+// Base model class
+class Model {
+	var $fields, $structure;
+	function Model(){
+		$this->fields = new Fields();
+	}
+}
+// Class of built-in fields.
+class Fields {
+	// -- CONCEPT --
+	// format defines how to format it. [integer, string]
+	// schema defines what schema to use in the CREATE. [`string` VARCHAR( 255 )% NULL%% DEFAULT %]
+	// - Note, %SOMETHING% will be replaced with the value you define. Spaces can be placed before 
+	//   and after the SOMETHING. They will only be shown if a SOMETHING is specified.
+	var $string = array(
+		'format' => 'string',
+		'schema' => 'string` VARCHAR( 255 )% NULL%% DEFAULT %',
+		'null' => array(
+			'enabled' => 'boolean',
+			'default' => 'NOT NULL',
+			'true' => 'NULL',
+			'false' => 'NOT NULL',
+		),
+		'default' => array(
+			'enabled' => 'string',
+			'format' => "'%STRING%'",
+		)
+	);
+	var $password = 'a password';
+	var $url = 'a URL';
+}
