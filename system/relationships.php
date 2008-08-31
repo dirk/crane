8dd1c28056ac370dd8a88433e161b5488f62d912
Dirk Gadsden <dirk@esherido.com>
2008-08-31 09:18:37 -0400

    Extended relationships concept

--- system/relationships.php
+++ system/relationships.php
@@ -1,8 +1,25 @@
 <?php
 class BelongsTo {
 	var $name, $parent;
-	function BelongsTo($parent, $name){
+	function BelongsTo($parent, $name, $autoload = true){
 		$this->parent = $parent;$this->name = $name;
+		// Auto-load tells it whether or not to auto-matically load the parent when the load() event is called.
+		$this->autoload = $autoload;
+	}
+	function load($id, $force = false){
+		if($this->autoload == true || $force == true){
+			// Load the data from the database
+			/*
+			$parent = new $this->parent();
+			$data = query('SELECT ' . build_schema($parent->structure) . ' FROM ' . $parent->name . ' WHERE id = ' . $id);
+			foreach($data as $key => $value){
+				$parent->$key = $value;
+			}
+			return $parent;
+			*/
+		}else{
+			return false;
+		}
 	}
 }
 
